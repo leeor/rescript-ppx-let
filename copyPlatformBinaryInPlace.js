52d4bc9c695646c72b880496f36fab45e297e7ae
Leeor Aharon <leeor.aharon@gmail.com>
2020-09-06 23:27:39 +0300

    initial commit

---
+++ copyPlatformBinaryInPlace.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+var fs = require("fs");
+var path = require("path");
+
+var arch = process.arch === "ia32" ? "x86" : process.arch;
+var platform = process.platform === "win32" ? "win" : process.platform;
+
+var rootDir = __dirname;
+var sourceExePath = path.join(rootDir, `ppx-${platform}-${arch}.exe`);
+var destExePath = path.join(rootDir, "ppx.exe");
+
+copyBinary(sourceExePath, destExePath);
+
+function copyBinary(sourceFilename, destFilename) {
+  var supported = fs.existsSync(sourceFilename);
+
+  if (!supported) {
+    console.error("rescript-ppx-let does not support this platform :(");
+    console.error("");
+    console.error("rescript-ppx-let comes prepacked as built binaries to avoid large");
+    console.error("dependencies at build-time.");
+    console.error("");
+    console.error("If you want rescript-ppx-let to support this platform natively,");
+    console.error(
+      "please open an issue at our repository, linked above. Please"
+    );
+    console.error("specify that you are on the " + platform + " platform,");
+    console.error("on the " + arch + " architecture.");
+  }
+
+  if (!fs.existsSync(destFilename)) {
+    copyFileSync(sourceFilename, destFilename);
+    fs.chmodSync(destFilename, 0755);
+  }
+}
+
+function copyFileSync(source, dest) {
+  if (typeof fs.copyFileSync === "function") {
+    fs.copyFileSync(source, dest);
+  } else {
+    fs.writeFileSync(dest, fs.readFileSync(source));
+  }
+}
